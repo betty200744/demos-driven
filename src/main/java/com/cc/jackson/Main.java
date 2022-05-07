422b82d5002a22505da7a00db56e761b5a11faef
Yuanli Zhao <yuanli.zhao@compass.com>
2022-05-07 09:40:33 +0800

    [feat] update jackson demo

--- src/main/java/com/cc/jackson/Main.java
+++ src/main/java/com/cc/jackson/Main.java
@@ -3,7 +3,6 @@ package com.cc.jackson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 public class Main {
     public static void main(String[] args) throws JsonProcessingException {
         studentFromJson();
@@ -13,15 +12,16 @@ public class Main {
     private static void studentFromJson() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         String stu = "{\"id\":1,\"name\":\"betty\"}";
-        Student student = mapper.readerFor(Student.class).readValue(stu);
-        System.out.println(student.name);
+        Student student0 = mapper.readValue(stu, Student.class);
+        Student student1 = mapper.readerFor(Student.class).readValue(stu);
+        System.out.println(student0.name);
+        System.out.println(student1.name);
     }
 
     private static void studentToJson() throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         Student student = new Student(1, "betty");
-        System.out.println(mapper.writeValueAsString(student));
+        String studentString = mapper.writeValueAsString(student);
+        System.out.println(studentString);
     }
-
-
 }
